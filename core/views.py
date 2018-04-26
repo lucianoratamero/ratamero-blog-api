5419625fcb0ee280369e9c9081862e4eed687cc7
Luciano Ratamero <luciano@ratamero.com>
2018-04-25 22:38:15 -0300

    adds page count to the list_posts.

--- core/views.py
+++ core/views.py
@@ -8,13 +8,18 @@ from core import models
 
 def list_posts(page: http.QueryParam):
     db_posts = models.BlogPost.objects.all()
+    page_count = 1
     if page:
         paginator = Paginator(db_posts, 5)
+        page_count = paginator.count
         try:
             db_posts = paginator.page(int(page))
         except EmptyPage:
             db_posts = []
-    return [schemas.BlogPost(db_post.__dict__) for db_post in db_posts]
+    return {
+        'page_count': page_count,
+        'posts': [schemas.BlogPost(db_post.__dict__) for db_post in db_posts]
+    }
 
 
 def post(slug: str):
