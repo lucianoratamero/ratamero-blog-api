7bfc392c9d3bfcff73851422de6ecd0a944d9eb2
Luciano Ratamero <luciano@ratamero.com>
2018-04-26 02:47:02 -0300

    fixes serialization.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 
 from apistar import http
+from markdownx.utils import markdownify
 from django.core.paginator import Paginator, EmptyPage
 
 from core import schemas
@@ -16,10 +17,15 @@ def list_posts(page: http.QueryParam):
             db_posts = paginator.page(int(page))
         except EmptyPage:
             db_posts = []
+
+    posts = [schemas.BlogPost(db_post.__dict__) for db_post in db_posts]
+    for post in posts:
+        post['body'] = markdownify(post['body'])
+
     return {
         'page_count': page_count,
         'current_page': page or 1,
-        'posts': [schemas.BlogPost(db_post.__dict__) for db_post in db_posts]
+        'posts': posts
     }
 
 
@@ -28,4 +34,8 @@ def post(slug: str):
         db_post = models.BlogPost.objects.get(slug=slug)
     except models.BlogPost.DoesNotExist:
         return http.Response({'message': 'Post {} does not exist.'.format(slug)}, status=404)
-    return schemas.BlogPost(db_post.__dict__)
+
+    post = schemas.BlogPost(db_post.__dict__)
+    post['body'] = markdownify(post['body'])
+
+    return post
